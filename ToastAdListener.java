6a07b3fdfa7af102c0153346a43cd8f1a5598236
Carmelito Andrade <carmelito0604@gmail.com>
2014-02-08 00:02:06 -0500

    Update toastAdlistner for admobs

--- ToastAdListener.java
+++ ToastAdListener.java
@@ -34,7 +34,7 @@ public class ToastAdListener extends AdListener {
 
     @Override
     public void onAdLoaded() {
-        Toast.makeText(mContext, "onAdLoaded()", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(mContext, "onAdLoaded()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -60,16 +60,16 @@ public class ToastAdListener extends AdListener {
 
     @Override
     public void onAdOpened() {
-        Toast.makeText(mContext, "onAdOpened()", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(mContext, "onAdOpened()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onAdClosed() {
-        Toast.makeText(mContext, "onAdClosed()", Toast.LENGTH_SHORT).show();
+       // Toast.makeText(mContext, "onAdClosed()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onAdLeftApplication() {
-        Toast.makeText(mContext, "onAdLeftApplication()", Toast.LENGTH_SHORT).show();
+       // Toast.makeText(mContext, "onAdLeftApplication()", Toast.LENGTH_SHORT).show();
     }
 }
